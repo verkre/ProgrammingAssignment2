0c5fad88f35668f7d471002c7041ffc1a6f87abd
verkre <vera@vmba.fritz.box>
2015-11-22 12:17:39 +0100

    r programming assignment #2
    implemented makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the function makeCacheMatrix creates an object with two private variables (myMatrix, invertedMatrix)
+## and allows working with them by calling one of four functions (set, get, setInverse, getInverse) 
+## defined within the object constructor makeCacheMatrix 
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(myMatrix = matrix()) {
+        invertedMatrix <- NULL
+        set <- function(newMatrix) {
+                myMatrix <<- newMatrix
+                invertedMatrix <<- NULL
+        }
+        get <- function() myMatrix
+        setInverse <- function(newInvertedMatrix) invertedMatrix <<- newInvertedMatrix
+        getInverse <- function() invertedMatrix
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## the function cacheSolve checks whether the inverse of a given CacheMatrix (x) was
+## already computed. If so, it returns the inverse. If not, the inverse is computed,
+## saved in x, and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invertedMatrix <- x$getInverse()
+        if(!is.null(invertedMatrix)) {
+                message("getting cached data")
+                return(invertedMatrix)
+        }
+        data <- x$get()
+        invertedMatrix <- solve(data, ...)
+        x$setInverse(invertedMatrix)
+        invertedMatrix
 }
